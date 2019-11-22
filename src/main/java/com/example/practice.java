634856237a3298a489a8a571c3047d4dadb379d5
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2019-11-22 10:42:08 +0900

    first commit

--- src/main/java/com/example/practice.java
+++ src/main/java/com/example/practice.java
@@ -2,4 +2,20 @@ package com.example;
 
 public class practice {
 
+	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
